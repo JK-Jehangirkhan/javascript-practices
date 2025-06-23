fbbb2da73f6045d719b2400a785c6edad2cfd2e2
Jehangir khan <jehangirkhanpk2@gmail.com>
2025-06-23 07:07:47 +0000

    my short intro

---
+++ BasicsPractice/01-basics.js
@@ -0,0 +1,8 @@
+
+console.log('hello everyone')
+// my simple intruduction 
+let myName ="Jehangir khan"
+let myProfession = "javascript developer"
+let MyProjects ="Social Media and professional networking websites"
+
+console.log(`this is me ${myName} and i am ${myProfession} i am working on ${MyProjects}`)
